0ec5de97c5e68431b20f32507497d02d3cf40ee0
Diego-Armando-H <diego.hernandez7503@alumnos.udg.mx>
2022-09-26 23:44:07 -0500

    primera

---
+++ ui_mainwindow.py
@@ -0,0 +1,115 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'mainwindow.ui'
+##
+## Created by: Qt User Interface Compiler version 5.15.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+from PySide2.QtWidgets import *
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(276, 246)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.groupBox = QGroupBox(self.centralwidget)
+        self.groupBox.setObjectName(u"groupBox")
+        self.groupBox.setGeometry(QRect(0, 0, 271, 201))
+        self.formLayout = QFormLayout(self.groupBox)
+        self.formLayout.setObjectName(u"formLayout")
+        self.label = QLabel(self.groupBox)
+        self.label.setObjectName(u"label")
+
+        self.formLayout.setWidget(0, QFormLayout.LabelRole, self.label)
+
+        self.lineEdit = QLineEdit(self.groupBox)
+        self.lineEdit.setObjectName(u"lineEdit")
+
+        self.formLayout.setWidget(0, QFormLayout.FieldRole, self.lineEdit)
+
+        self.label_2 = QLabel(self.groupBox)
+        self.label_2.setObjectName(u"label_2")
+
+        self.formLayout.setWidget(1, QFormLayout.LabelRole, self.label_2)
+
+        self.lineEdit_2 = QLineEdit(self.groupBox)
+        self.lineEdit_2.setObjectName(u"lineEdit_2")
+
+        self.formLayout.setWidget(1, QFormLayout.FieldRole, self.lineEdit_2)
+
+        self.label_3 = QLabel(self.groupBox)
+        self.label_3.setObjectName(u"label_3")
+
+        self.formLayout.setWidget(2, QFormLayout.LabelRole, self.label_3)
+
+        self.lineEdit_3 = QLineEdit(self.groupBox)
+        self.lineEdit_3.setObjectName(u"lineEdit_3")
+
+        self.formLayout.setWidget(2, QFormLayout.FieldRole, self.lineEdit_3)
+
+        self.label_5 = QLabel(self.groupBox)
+        self.label_5.setObjectName(u"label_5")
+
+        self.formLayout.setWidget(3, QFormLayout.LabelRole, self.label_5)
+
+        self.label_6 = QLabel(self.groupBox)
+        self.label_6.setObjectName(u"label_6")
+
+        self.formLayout.setWidget(4, QFormLayout.LabelRole, self.label_6)
+
+        self.label_7 = QLabel(self.groupBox)
+        self.label_7.setObjectName(u"label_7")
+
+        self.formLayout.setWidget(5, QFormLayout.LabelRole, self.label_7)
+
+        self.spinBox_2 = QSpinBox(self.groupBox)
+        self.spinBox_2.setObjectName(u"spinBox_2")
+        self.spinBox_2.setMaximum(255)
+
+        self.formLayout.setWidget(4, QFormLayout.FieldRole, self.spinBox_2)
+
+        self.spinBox = QSpinBox(self.groupBox)
+        self.spinBox.setObjectName(u"spinBox")
+        self.spinBox.setMaximum(255)
+
+        self.formLayout.setWidget(3, QFormLayout.FieldRole, self.spinBox)
+
+        self.spinBox_3 = QSpinBox(self.groupBox)
+        self.spinBox_3.setObjectName(u"spinBox_3")
+        self.spinBox_3.setMaximum(255)
+
+        self.formLayout.setWidget(5, QFormLayout.FieldRole, self.spinBox_3)
+
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QMenuBar(MainWindow)
+        self.menubar.setObjectName(u"menubar")
+        self.menubar.setGeometry(QRect(0, 0, 276, 26))
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QStatusBar(MainWindow)
+        self.statusbar.setObjectName(u"statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.groupBox.setTitle(QCoreApplication.translate("MainWindow", u"GroupBox", None))
+        self.label.setText(QCoreApplication.translate("MainWindow", u"Destino X", None))
+        self.label_2.setText(QCoreApplication.translate("MainWindow", u"Destino Y", None))
+        self.label_3.setText(QCoreApplication.translate("MainWindow", u"Velocidad", None))
+        self.label_5.setText(QCoreApplication.translate("MainWindow", u"Red", None))
+        self.label_6.setText(QCoreApplication.translate("MainWindow", u"Green", None))
+        self.label_7.setText(QCoreApplication.translate("MainWindow", u"Blue", None))
+    # retranslateUi
+
